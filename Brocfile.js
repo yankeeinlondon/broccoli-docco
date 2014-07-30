a08354559f50f0a5d868247e549d2b7982477bec
Ken Snyder <ken@ken.net>
2014-07-30 13:25:36 +0100

    more complete testing

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,18 @@
 // ##Brocfile##
 // this `Brocfile` is used by the *broccoli-docco* unit tests
 
-var docco = require('./index.js');
-var documents = docco('fixtures'); // note: output refers to both where Docco will put output files, and tree offset returned to Brocolli
+var docco = require('./index.js'),
+	pickFiles = require('broccoli-static-compiler'),
+	mergeTrees = require('broccoli-merge-trees');
+
+// Add the Brocfile to the output of the test
+var rootJs = pickFiles('.',{
+	srcDir: '/',
+	files: ['Brocfile.js'],
+	destDir: '/'
+});
+// Primary output is the contents of the fixtures directory
+var source = 'fixtures';
+var documents = docco(mergeTrees([source,rootJs]));
 
 module.exports = documents;
